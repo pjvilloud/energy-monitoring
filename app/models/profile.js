29b37547433e68a89ac38da854afe8d3be3d0915
pijey <pjvilloud-bitbucket@yahoo.fr>
2018-03-10 16:14:10 +0100

    Add validation general info

--- app/models/profile.js
+++ app/models/profile.js
@@ -1,6 +1,38 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+import { validator, buildValidations } from 'ember-cp-validations';
+
+const Validations = buildValidations({
+  username: [
+    validator('presence', true),
+    validator('length', {
+      max: 100
+    }),
+    validator(function(value) {
+      return true;
+    })
+  ],
+  language: [
+    validator('presence', true),
+  ],
+  currency: [
+    validator('presence', true),
+  ],
+  country:  [
+    validator('presence', true),
+    validator('length', {
+      max: 100
+    })
+  ],
+  city:  [
+    validator('presence', true),
+    validator('length', {
+      max: 100
+    })
+  ]
+});
+
+export default DS.Model.extend(Validations, {
   username: DS.attr("string"),
   language: DS.attr("string"),
   country: DS.attr("string"),
